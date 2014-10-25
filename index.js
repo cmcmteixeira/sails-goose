6b808e173b590fd01ac393b9415ff551175bcdd1
Papa Formigas <cmcmteixeira+gitlab@gmail.com>
2014-10-25 19:13:36 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,41 @@
+	var fs 		= require('fs');
+	var root  	= process.cwd();
+	var mongoose= require('mongoose');
+	var goose 	= null; 
+
+	var schemaFolder = root + '/api/goose-models/';
+	var configFile   = root + '/config/goose.js'; 
+
+	//------------------------------
+	var schemaFile = fs.readdirSync( process.cwd() +'/api/goose-models' )
+
+	//------------------------------
+	var schemas = [];
+	for ( i in schemaFile ){
+		schemas.push( { 
+			name 	: schemaFile[i].replace(/\.js$/i,'') , 
+			schema 	: require( schemaFolder + schemaFile[i] )  } )
+	}
+
+	//------------------------------
+	var config;
+	config = require( configFile ).goose;
+
+	//------------------------------
+	var options = config.options;
+	goose = mongoose.connect('mongodb://'+config.host + ":" + config.port+'/' + config.db , options);
+
+	//------------------------------
+	for ( i in schemas){
+		goose[ schemas[i].name ] = goose.model( schemas[i].name , schemas[i].schema ,  schemas[i].name );
+	}
+
+
+	//------------------------------
+	if ( config.globals ){
+		for ( i in schemas ){
+			global[schemas[i].name] = goose[schemas[i].name]
+		}
+	}
+	
+	global.goose = goose; 
