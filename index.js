bed1b2b1696448a1c34ecdbd2333c5ee9686b82f
Papa Formigas <cmcmteixeira+gitlab@gmail.com>
2014-11-06 16:16:10 +0000

    Updated Readme + The script now supports database choosing

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+
+module.exports = function( db ){
+	
+
 	var fs 		= require('fs');
 	var root  	= process.cwd();
 	var mongoose= require('mongoose');
-	var goose 	= null; 
+	var goose 	= {};
+	var goose_ 	= null; 
 
 	var schemaFolder = root + '/api/goose-schemas/';
 	var configFile   = root + '/config/goose.js'; 
 
+	// Iterating through the schema file and obtaining the schemas
 	//------------------------------
 	var schemaFile = fs.readdirSync( schemaFolder )
-	//------------------------------
 	var schemas = [];
 	for ( i in schemaFile ){
 		schemas.push( { 
@@ -17,25 +22,29 @@
 		} );
 	}
 
-	//------------------------------
+	//Reading the configurations
+	//--------------------------
 	var config;
 	config = require( configFile ).goose;
 
-	//------------------------------
+	//Obtaining connection options
+	//----------------------------
 	var options = config.options;
-	goose = mongoose.connect('mongodb://'+config.host + ":" + config.port+'/' + config.db , options);
+	goose_ = mongoose.connect('mongodb://'+config[db].host + ":" + config[db].port+'/' + config[db].db , options);
 
-	//------------------------------
+	// Creating models base on the db passed
+	//--------------------------------------
 	for ( i in schemas){
-		goose[ schemas[i].name ] = goose.model( schemas[i].name , schemas[i].schema ,  schemas[i].name );
+		goose[ schemas[i].name ] = goose_.model( schemas[i].name , schemas[i].schema ,  schemas[i].name );
 	}
 
-
+	// Setting models as globals
 	//------------------------------
 	if ( config.globals ){
 		for ( i in schemas ){
 			global[schemas[i].name] = goose[schemas[i].name]
 		}
 	}
-	
+
 	global.goose = goose; 
+}
